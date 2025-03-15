12dc9596542847e2f80b5192d34426c05ccb5132
Maycon de Oliveira <maycon.carneiro@quality.com.br>
2025-03-15 12:49:48 -0300

    Intermediarios

--- AULA-ROTAS-INTERMEDIARIOS/src/index.js
+++ AULA-ROTAS-INTERMEDIARIOS/src/index.js
@@ -1,36 +1,21 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-const professores = [
-    {id: 1, nome: 'Guido', stack: 'Backend'},
-    {id: 2, nome: 'Dani', stack: 'Frontend'},
-    {id: 3, nome: 'Diego', stack: 'Frontend'},
-    {id: 4, nome: 'Vidal', stack: 'Backend'}
-]
+const { filtrarProfessores, encontrarUmProfessor } = require('./controladores/professores')
+
+
+// para informar que vamos utilizar os intermediários/Middleware
+app.use((req, res, next) => {
+
+}); 
 
 //http://localhost:3000/professores
-app.get('/professores', (req, res) => {
-    const { stack } = req.query //desestruturação
-
-    let resultado = professores;
-
-    if (stack){
-        resultado = professores.filter((professor) => {
-            return professor.stack === stack
-        })
-    }
-
-    res.send(resultado)
-});
+app.get("/professores", filtrarProfessores);
 
 //http://localhost:3000/professores/3
-app.get('/professores/:id', (req, res) => {
-    const professorEncontrado = professores.find((professor) => {
-        return professor.id === Number(req.params.id)
-    })
+app.get("/professores/:id", encontrarUmProfessor);
 
-    res.send(professorEncontrado)
-});
+
 
 app.listen(3000);
